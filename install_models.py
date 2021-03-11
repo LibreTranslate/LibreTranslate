e0693d697e65e6d620433c7476f8bca5f15cc7b7
mammo0 <marc.ammon@hotmail.de>
2021-03-11 12:33:54 +0100

    download the transliteration packages of polyglot during boot

--- install_models.py
+++ install_models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from app.init import check_and_install_models
+from app.init import check_and_install_models, check_and_install_transliteration
 
 if __name__ == "__main__":
 	check_and_install_models(force=True)
+	check_and_install_transliteration(force=True)
