e828658332e01dd49e8ae25a0cd1d99fda6ee4b8
Piti Cookie <piti_cookie@helilot.com>
2022-03-31 07:21:20 -0700

    Added models include option in docker build

--- install_models.py
+++ install_models.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
-
+import argparse
 from app.init import check_and_install_models, check_and_install_transliteration
 
 if __name__ == "__main__":
-    check_and_install_models(force=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--load_only_lang_codes", type=str, default="")
+    args = parser.parse_args()
+    lang_codes = args.load_only_lang_codes.split(",") or None
+    check_and_install_models(force=True, load_only_lang_codes=lang_codes)
     check_and_install_transliteration(force=True)
