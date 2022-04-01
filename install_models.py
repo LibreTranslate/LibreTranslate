8cc03572e53a0be3d6ce55c46b76b4c32882f5ed
Piero Toffanin <pt@masseranolabs.com>
2022-04-01 10:01:15 -0400

    Fix install_models.py

--- install_models.py
+++ install_models.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--load_only_lang_codes", type=str, default="")
     args = parser.parse_args()
-    lang_codes = args.load_only_lang_codes.split(",") or None
+    lang_codes = args.load_only_lang_codes.split(",")
+    if len(lang_codes) == 0:
+        lang_codes = None
     check_and_install_models(force=True, load_only_lang_codes=lang_codes)
     check_and_install_transliteration(force=True)
