99c6ceba43e36ec39fba5f6ba54d478b4e3eba10
Sébastien Thuret <contact@sebastien-thuret.fr>
2022-04-03 19:50:11 +0200

    upgrade pytest

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=open("requirements.txt", "r").readlines(),
-    tests_require=['pytest==5.2.0'],
+    tests_require=['pytest==7.1.1'],
     setup_requires=['pytest-runner'],
     classifiers=[
         "License :: OSI Approved :: GNU Affero General Public License v3 ",
