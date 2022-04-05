7113a30a0c932ea5c24ad7ceee0a5a386af1b682
Sébastien Thuret <contact@sebastien-thuret.fr>
2022-04-05 16:39:31 +0200

    load only en & es models (avoid to download all models)

--- tests/test_api/conftest.py
+++ tests/test_api/conftest.py
@@ -2,12 +2,14 @@ import sys
 import pytest
 
 from app.app import create_app
+from app.default_values import DEFAULT_ARGUMENTS
 from app.main import get_args
 
 
 @pytest.fixture()
 def app():
     sys.argv = ['']
+    DEFAULT_ARGUMENTS['LOAD_ONLY'] = "en,es"
     app = create_app(get_args())
 
     yield app
