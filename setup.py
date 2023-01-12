84fbd41501525efa3ed8a1e68e929dcdace1f5c0
dingedi <contact@dingedi.com>
2023-01-12 14:50:38 +0100

    upgrade pytest

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=open("requirements.txt", "r").readlines(),
-    tests_require=['pytest==7.1.2'],
+    tests_require=['pytest==7.2.0'],
     setup_requires=['pytest-runner'],
     classifiers=[
         "License :: OSI Approved :: GNU Affero General Public License v3 ",
