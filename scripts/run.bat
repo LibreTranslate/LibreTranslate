6e04c88308886f01140df1474630dfe38e284e4e
dingedi <contact@dingedi.com>
2023-01-12 14:21:17 +0100

    mover scripts in a scripts dir

--- scripts/run.bat
+++ scripts/run.bat
@@ -11,7 +11,7 @@ IF NOT "%1"=="" (
         SHIFT
     )
     IF "%1"=="--help" (
-        echo Usage: run.bat [--port N]
+        echo Usage: scripts/run.bat [--port N]
         echo:
         echo Run LibreTranslate using docker.
         echo:
@@ -33,7 +33,7 @@ docker run -ti --rm -p %LT_PORT%:%LT_PORT% %DB_VOLUME% -v lt-local:/home/libretr
 GOTO :done
 
 :install_docker
-ECHO Cannot find docker! Go to https://docs.docker.com/desktop/install/windows-install/ and install docker before running this script (pressing Enter will open the page) 
+ECHO Cannot find docker! Go to https://docs.docker.com/desktop/install/windows-install/ and install docker before running this script (pressing Enter will open the page)
 pause
 start "" https://docs.docker.com/desktop/install/windows-install/
 GOTO :done
