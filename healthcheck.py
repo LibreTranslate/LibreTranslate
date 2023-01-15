83282ab47d3ca1e4d7e446d3c21fc7be491f5540
Jon Chin <jon@sharebyteaching.org>
2023-01-15 16:21:13 -0500

    small script for testing if service is ready for requests

---
+++ healthcheck.py
@@ -0,0 +1,11 @@
+import requests
+response = requests.post(
+    url='http://0.0.0.0:5000/translate',
+    headers={'Content-Type': 'application/json'},
+    json={
+         'q': 'Hello World!',
+         'source': 'en',
+         'target': 'en'
+    }
+)
+# if server unavailable then requests with raise exception and healthcheck will fail
