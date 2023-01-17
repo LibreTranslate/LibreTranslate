e7f29d21940b1ecac9e4ef9c896e51b7e25efec2
Joshix <joshix.dev@mailbox.org>
2023-01-17 19:26:52 +0100

    refactor the detector to inherit from a BaseDetector abc

--- libretranslate/detect.py
+++ libretranslate/detect.py
@@ -1,5 +1,7 @@
 # Originally adapted from https://github.com/aboSamoor/polyglot/blob/master/polyglot/base.py
 
+from abc import ABC
+
 import pycld2 as cld2
 
 class UnknownLanguage(Exception):
@@ -23,50 +25,62 @@ class Language(object):
     return Language(("", code, 100, 0))
 
 
-class Detector(object):
-  """ Detect the language used in a snippet of text."""
+class BaseDetector(ABC):
+  """Detect the language used in a snippet of text."""
 
-  def __init__(self, text, quiet=False):
+  def __init__(self, text: str, quiet: bool = False) -> None:
     """ Detector of the language used in `text`.
     Args:
       text (string): unicode string.
     """
-    self.__text = text
+    # self.__text = text
     self.reliable = True
     """False if the detector used Best Effort strategy in detection."""
     self.quiet = quiet
     """If true, exceptions will be silenced."""
-    self.detect(text)
-
-  @staticmethod
-  def supported_languages():
-    """Returns a list of the languages that can be detected by pycld2."""
-    return [name.capitalize() for name,code in cld2.LANGUAGES if not name.startswith("X_")]
-
-  def detect(self, text):
-    """Decide which language is used to write the text.
-    The method tries first to detect the language with high reliability. If
-    that is not possible, the method switches to best effort strategy.
-    Args:
-      text (string): A snippet of text, the longer it is the more reliable we
-                     can detect the language used to write the text.
-    """
-    reliable, index, top_3_choices = cld2.detect(text, bestEffort=False)
-
-    if not reliable:
-      self.reliable = False
-      reliable, index, top_3_choices = cld2.detect(text, bestEffort=True)
-      
-      if not self.quiet:
-        if not reliable:
-          raise UnknownLanguage("Try passing a longer snippet of text")
-
-    self.languages = [Language(x) for x in top_3_choices]
+    self.languages = self.detect(text)
     self.language = self.languages[0]
-    return self.language
 
-  def __str__(self):
+  def __str__(self) -> str:
     text = "Prediction is reliable: {}\n".format(self.reliable)
     text += u"\n".join(["Language {}: {}".format(i+1, str(l))
                         for i,l in enumerate(self.languages)])
     return text
+
+  @staticmethod
+  def supported_languages() -> "list[str]":
+    """Returns a list of the languages that this Detector can detect."""
+    raise NotImplementedError()
+
+  def detect(self, text: str) -> "list[Language]":
+    """Decide which language is used to write the text."""
+    raise NotImplementedError()
+
+
+class Detector(BaseDetector):
+    """ Detect the language used in a snippet of text."""
+
+    @staticmethod
+    def supported_languages() -> "list[str]":
+        """Returns a list of the languages that can be detected by pycld2."""
+        return [name.capitalize() for name, code in cld2.LANGUAGES if not name.startswith("X_")]
+
+    def detect(self, text) -> "list[Language]":
+        """Decide which language is used to write the text.
+        The method tries first to detect the language with high reliability. If
+        that is not possible, the method switches to the best effort strategy.
+        Args:
+          text (string): A snippet of text, the longer it is the more reliable we
+                         can detect the language used to write the text.
+        """
+        reliable, index, top_3_choices = cld2.detect(text, bestEffort=False)
+
+        if not reliable:
+            self.reliable = False
+            reliable, index, top_3_choices = cld2.detect(text, bestEffort=True)
+
+            if not self.quiet:
+                if not reliable:
+                    raise UnknownLanguage("Try passing a longer snippet of text")
+
+        return [Language(x) for x in top_3_choices]
