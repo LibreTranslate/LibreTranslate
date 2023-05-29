fdb317c5595a5d557e142a369b945c307ba6dd9e
Piero Toffanin <pt@masseranolabs.com>
2023-05-29 16:47:32 -0400

    Don't force ASCII output

--- libretranslate/app.py
+++ libretranslate/app.py
@@ -1058,6 +1058,7 @@ def create_app(args):
 
     app.config["SESSION_TYPE"] = "filesystem"
     app.config["SESSION_FILE_DIR"] = os.path.join("db", "sessions")
+    app.config["JSON_AS_ASCII"] = False
     Session(app)
 
     if args.debug:
