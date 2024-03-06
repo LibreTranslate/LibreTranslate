b25ed7aae475f5452cce3dbecc4ab5839e93cd66
Cyril Levis <git@levis.name>
2024-03-06 10:37:47 +0100

    fix: make libretranslate with ipv6

--- scripts/healthcheck.py
+++ scripts/healthcheck.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.post(
-    url='http://0.0.0.0:5000/translate',
+    url='http://[::1]:5000/translate',
     headers={'Content-Type': 'application/json'},
     json={
          'q': 'Hello World!',
