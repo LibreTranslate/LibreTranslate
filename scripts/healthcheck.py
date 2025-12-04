9cb966c3f6090decc317d86b55fb850f628aa26d
Piero Toffanin <pt@masseranolabs.com>
2025-12-04 00:49:31 -0500

    Use health endpoint in check health

--- scripts/healthcheck.py
+++ scripts/healthcheck.py
@@ -2,14 +2,10 @@ import requests
 import os
 
 port = os.environ.get('LT_PORT', '5000')
-response = requests.post(
-    url=f'http://localhost:{port}/translate',
+response = requests.get(
+    url=f'http://localhost:{port}/health',
     headers={'Content-Type': 'application/json'},
-    json={
-         'q': 'Hello World!',
-         'source': 'en',
-         'target': 'en'
-    },
+    json={},
     timeout=60
 )
 response.raise_for_status() # if server unavailable then requests with raise exception and healthcheck will fail
